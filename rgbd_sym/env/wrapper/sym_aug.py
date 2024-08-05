15f3466b13e0db290dda9d95a35daaa58437bce4
Ben <522181941@qq.com>
2024-08-05 17:16:47 +0800

    fixing "Killed" bug
    fix done

--- rgbd_sym/env/wrapper/sym_aug.py
+++ rgbd_sym/env/wrapper/sym_aug.py
@@ -1,7 +1,8 @@
 from rgbd_sym.env.wrapper.base import BaseWrapper
 from rgbd_sym.tool.sym_tool import get_random_transform_params, perturb
 import sys
-
+import numpy as np
+from copy import deepcopy
 
 class SymAug(BaseWrapper):
 
@@ -12,9 +13,9 @@ class SymAug(BaseWrapper):
         super().__init__(env)
         self._sym_num = sym_num
         self._eps_idx = 0
-        self._eps_data = None
         self._timestep = 0
-        self._data_collect_phase= False
+        self._filling_eps_data = True
+        self._empty_eps_data()
 
     def _empty_eps_data(self):
         self._eps_data = {"reset": None, "step":[],}
@@ -43,42 +44,64 @@ class SymAug(BaseWrapper):
 
     def reset(self, ):
         self._timestep = 0
-        self._eps_idx +=1
-        self._data_collect_phase =  self._eps_idx % self._sym_num == 1 or self._eps_data is None
-        if self._data_collect_phase:
-            self._empty_eps_data()
-            obs =  self.env.reset()
-            self._eps_data["reset"] = obs
+        self._eps_idx += 1 
+        self._timestep += 1
+        self._filling_eps_data = self._eps_data["reset"] is not None
 
-            return obs
-
-        else:
-            obs = self._eps_data["reset"]
+        self._eps_idx = self._eps_idx % (self.SymNum + 1) # prevent idx explode
+        if self._check_sym_condition():
+            obs = deepcopy(self._eps_data["reset"])
             theta, trans, pivot = get_random_transform_params(obs["image"].shape)
             self._transform_param = (theta, trans, pivot,)
             new_obs, _ = self._transform_obs(obs)
             return new_obs
+        else:
+            self._empty_eps_data()
+            obs =  self.env.reset()
+            if self.env.mode =="train": # do not update when evaluate env
+                self._eps_data["reset"] = obs
+            return obs
 
     def step(self, action):
-        self._timestep+=1
-        if self._data_collect_phase:
+
+        if not self._check_sym_condition():
             step_r =  self.env.step(action)
-            self._eps_data["step"].append(step_r + (action, ))
+            if self.env.mode =="train": # do not update when evaluate env
+                self._eps_data["step"].append(step_r + (action, ))
+            self._timestep += 1
             return step_r
         else:
-            obs, reward, done, info, action_  = self._eps_data["step"][self._timestep-1]
+            _idx = self._timestep - 1
+            if _idx >= len(self._eps_data["step"]) - 1:
+                _idx = len(self._eps_data["step"]) - 1
+                done = True
+            else:
+                done = False
+            # print(_idx, len(self._eps_data["step"]))
+            obs, reward, _, info, action_ = deepcopy(self._eps_data["step"][_idx])
             new_obs, new_action = self._transform_obs(obs, action_)
             info['action'] = new_action
+            self._timestep += 1
             return new_obs, reward, done, info
 
     def get_sym_action(self):
-        obs, reward, done, info, action_ = self._eps_data["step"][self._timestep]
+        _idx = self._timestep - 1
+        if _idx >= len(self._eps_data["step"]) - 1:
+            _idx = len(self._eps_data["step"]) - 1
+        obs, reward, done, info, action_ = self._eps_data["step"][_idx]
         new_obs, new_action = self._transform_obs(obs, action_, action_only=True)
         return new_action
 
     @property
     def IsSymEnv(self):
-        return not self._data_collect_phase
+        return self._check_sym_condition()
+
+    def _check_sym_condition(self):
+        return (
+            self._eps_idx % (self.SymNum + 1) != 1
+            and self.env.mode == "train"
+            and self._filling_eps_data
+        )
 
     @property
     def SymNum(self):
