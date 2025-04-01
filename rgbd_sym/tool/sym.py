6a7431b2b12319cd44701b972833adde7ce72121
Ben <522181941@qq.com>
2025-04-01 22:38:05 +0800

    rot 1

--- rgbd_sym/tool/sym.py
+++ rgbd_sym/tool/sym.py
@@ -38,7 +38,7 @@ def get_sym_params(env_name):
         params['screw_angle'] =  np.random.uniform(0,10)
         params['sym_z_distance_thres'] = 0.092
 
-        params['rot_noise_ratio'] = 0.3
+        params['rot_noise_ratio'] = 1
         params['gripper_state_noise'] = False
 
         # if env_name in "block_push":
