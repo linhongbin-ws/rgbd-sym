d7c07f3ce7c97f8f9316dd08713b078c94f4d153
Ben <522181941@qq.com>
2025-05-13 12:34:13 +0800

    fix

--- bash/baseline/block_pick_our.sh
+++ bash/baseline/block_pick_our.sh
@@ -1,7 +1,7 @@
 source bash/init.sh
 gpu_id=${1:-0}
 seed=${2:-0}
-demon=${3:80}
+demon=${3:-80}
 CUDA_VISIBLE_DEVICES=${gpu_id} python ./rgbd_sym/rl/rsac/main.py \
  --cfg ./rgbd_sym/rl/rsac/configs/block_pick/rnn-equi-all.yml \
  --algo sac --seed ${seed} --cuda 0 --num_expert_episodes ${demon} \
