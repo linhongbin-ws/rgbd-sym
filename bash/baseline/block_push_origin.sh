ff838500eba699e43c1fa80671b5345bf080e79c
Ben <522181941@qq.com>
2025-05-13 12:53:57 +0800

    add prefix

--- bash/baseline/block_push_origin.sh
+++ bash/baseline/block_push_origin.sh
@@ -1,8 +1,9 @@
 source bash/init.sh
-gpu_id=${1:-0}
-seed=${2:-0}
-demon=${3:-80}
+prefix=${1:-" "}
+gpu_id=${2:-0}
+seed=${3:-0}
+demon=${4:-80}
 CUDA_VISIBLE_DEVICES=${gpu_id} python ./rgbd_sym/rl/rsac/main.py \
  --cfg ./rgbd_sym/rl/rsac/configs/block_push/rnn-equi-all.yml \
  --algo sac --seed ${seed} --cuda 0 --num_expert_episodes ${demon} \
- --sym_expert 0 --sym_normal 0 --traj_batch 0 --prefix bmt
+ --sym_expert 0 --sym_normal 0 --traj_batch 0 --prefix ${prefix}
