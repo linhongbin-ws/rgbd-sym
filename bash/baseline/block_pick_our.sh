21ed3b0a1fa0c37474e17c31c996d04596042208
ben <522181941@qq.com>
2025-05-13 12:24:42 +0800

    add baseline scripts

---
+++ bash/baseline/block_pick_our.sh
@@ -0,0 +1,8 @@
+source bash/init.sh
+gpu_id=${1:-0}
+seed=${2:-0}
+demon=${3:80}
+CUDA_VISIBLE_DEVICES=${gpu_id} python ./rgbd_sym/rl/rsac/main.py \
+ --cfg ./rgbd_sym/rl/rsac/configs/block_pick/rnn-equi-all.yml \
+ --algo sac --seed ${seed} --cuda 0 --num_expert_episodes ${demon} \
+ --sym_expert 4 --sym_normal 0 --traj_batch 3 --prefix bmt
